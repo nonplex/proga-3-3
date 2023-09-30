0506eeaf42acd4831019661fd2d3c3356187035f
nonplex <fuksvladislav@gmail.com>
2023-10-01 02:04:24 +0700

    Добавьте файлы проекта.

---
+++ 214/214.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+#include <locale>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	double a = 1.2;
+	double b = 4.9;
+	double x;//Произвольный x
+	double y;//Ответ
+	cout << "x = "; cin >> x;
+	if (x <= 2) //В случае первого условия 
+	{
+		y = ((a + x) / (1 + sqrt(x)));
+	}
+	else if ((2 < x) && (x < 5))//В случае второго условия
+	{
+		y = (atan(b / (x * x + 1)));
+	}
+	else {
+		y = (log10(x + sqrt(a * x)));//В случае третьего условия
+	}
+	cout << "y =" << y;
+}
